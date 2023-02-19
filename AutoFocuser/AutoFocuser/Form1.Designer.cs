275e488ce9e5d219925a2cc6d6e014e57ac447f6
Radosław Micał <micalradoslaw@gmail.com>
2023-02-19 16:51:35 +0100

    Added convertion of .cr2 images to bitmap, applying threshold filters to bitmaps. Implemented Convex Hull Algorithm to find perimeters of stars on image

--- AutoFocuser/AutoFocuser/Form1.Designer.cs
+++ AutoFocuser/AutoFocuser/Form1.Designer.cs
@@ -29,15 +29,25 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.goButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.stepsTextBox = new System.Windows.Forms.TextBox();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.HFDLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // goButton
             // 
-            this.goButton.Location = new System.Drawing.Point(335, 253);
+            this.goButton.Location = new System.Drawing.Point(42, 194);
             this.goButton.Name = "goButton";
             this.goButton.Size = new System.Drawing.Size(75, 23);
             this.goButton.TabIndex = 0;
@@ -48,7 +58,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(178, 144);
+            this.label1.Location = new System.Drawing.Point(12, 141);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 16);
             this.label1.TabIndex = 1;
@@ -56,27 +66,94 @@
             // 
             // stepsTextBox
             // 
-            this.stepsTextBox.Location = new System.Drawing.Point(335, 138);
+            this.stepsTextBox.Location = new System.Drawing.Point(156, 141);
             this.stepsTextBox.Name = "stepsTextBox";
             this.stepsTextBox.Size = new System.Drawing.Size(100, 22);
             this.stepsTextBox.TabIndex = 2;
             // 
             // serialPort1
             // 
-            this.serialPort1.PortName = "COM5";
+            this.serialPort1.PortName = "COM6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(17, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(393, 23);
+            this.connectButton.TabIndex = 4;
+            this.connectButton.Text = "Connect to SerialPort";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // HFDLabel
+            // 
+            this.HFDLabel.AutoSize = true;
+            this.HFDLabel.Location = new System.Drawing.Point(301, 197);
+            this.HFDLabel.Name = "HFDLabel";
+            this.HFDLabel.Size = new System.Drawing.Size(44, 16);
+            this.HFDLabel.TabIndex = 5;
+            this.HFDLabel.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(71, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(4288, 2854);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(487, 172);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(206, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(85, 333);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1272, 530);
+            this.panel1.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1641, 914);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.HFDLabel);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.stepsTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.goButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +165,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox stepsTextBox;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label HFDLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- AutoFocuser/AutoFocuser/Form1.cs
+++ AutoFocuser/AutoFocuser/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AForge.Imaging.Filters;
+using AForge.Imaging;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+using System.Security.Cryptography;
+using AForge.Math.Geometry;
+using AForge;
+using System.Windows;
 
 namespace AutoFocuser
 {
@@ -24,12 +31,88 @@ namespace AutoFocuser
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            serialPort1.Open();
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 4;
+            trackBar1.SmallChange = 1;
+            trackBar1.LargeChange = 1;
+            trackBar1.UseWaitCursor = false;
+            this.DoubleBuffered= true;
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             serialPort1.Close();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Load the BMP image from file
+            Bitmap image = new Bitmap("C:\\Users\\mical\\Desktop\\test2.bmp");
+            ProcessImage(image);
+        }
+
+
+        PictureBox org = new PictureBox();
+        private void ProcessImage(Bitmap image)
+        {
+            Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Threshold thresholdFilter = new Threshold(100);
+            Bitmap grayImage = grayscaleFilter.Apply(image);
+            Bitmap thresholdImage = thresholdFilter.Apply(grayImage);
+
+            // Apply blob counter to detect objects in the image
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 10;
+            blobCounter.MinWidth = 10;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size; 
+            blobCounter.ProcessImage(thresholdImage);
+
+            SimpleShapeChecker simpleShapeChecker = new SimpleShapeChecker();
+            GrahamConvexHull grahamConvexHull= new GrahamConvexHull();
+
+            // Get information about objects in the image
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+                List<IntPoint> hull = grahamConvexHull.FindHull(edgePoints);
+                Graphics g = Graphics.FromImage(image);
+                g.DrawPolygon(new Pen(Color.Red, 2), hull.Select(p => new PointF(p.X, p.Y)).ToArray());
+            }
+            // Update picture box
+            pictureBox1.Image = image;
+            org.Image = image;
+            image.Save("C:\\Users\\mical\\Desktop\\testOutput1.bmp");
+            panel1.AutoScrollPosition = new System.Drawing.Point((int)blobs[0].CenterOfGravity.X-panel1.Width/2, (int)blobs[0].CenterOfGravity.Y-panel1.Height/2);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value != 0)
+            {
+                pictureBox1.Image = null;
+                pictureBox1.Image = ZoomPicture(org.Image, new System.Drawing.Size(trackBar1.Value, trackBar1.Value));
+            }
+        }
+
+        System.Drawing.Image ZoomPicture(System.Drawing.Image img, System.Drawing.Size size)
+        {
+            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width * size.Width),
+                Convert.ToInt32(img.Height * size.Height));
+            Graphics g = Graphics.FromImage(bm);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bm;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
