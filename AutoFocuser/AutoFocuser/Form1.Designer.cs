cb5471381630c4a9473b5ee2b86d7670e95ad60b
Radosław Micał <micalradoslaw@gmail.com>
2023-02-21 00:32:08 +0100

    Modified way of calculating HFD from an image, added cropping image to smaller size and graphics visualization of both reference circles of hfd calculation and also marking star's circumference

--- AutoFocuser/AutoFocuser/Form1.Designer.cs
+++ AutoFocuser/AutoFocuser/Form1.Designer.cs
@@ -29,14 +29,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.goButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.stepsTextBox = new System.Windows.Forms.TextBox();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.connectButton = new System.Windows.Forms.Button();
-            this.HFDLabel = new System.Windows.Forms.Label();
+            this.hfdLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -95,21 +94,20 @@
             this.connectButton.UseVisualStyleBackColor = true;
             this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
-            // HFDLabel
+            // hfdLabel
             // 
-            this.HFDLabel.AutoSize = true;
-            this.HFDLabel.Location = new System.Drawing.Point(301, 197);
-            this.HFDLabel.Name = "HFDLabel";
-            this.HFDLabel.Size = new System.Drawing.Size(44, 16);
-            this.HFDLabel.TabIndex = 5;
-            this.HFDLabel.Text = "label2";
+            this.hfdLabel.AutoSize = true;
+            this.hfdLabel.Location = new System.Drawing.Point(503, 231);
+            this.hfdLabel.Name = "hfdLabel";
+            this.hfdLabel.Size = new System.Drawing.Size(44, 16);
+            this.hfdLabel.TabIndex = 5;
+            this.hfdLabel.Text = "label2";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(71, 9);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(4288, 2854);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -128,19 +126,19 @@
             this.panel1.AutoScroll = true;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(85, 333);
+            this.panel1.Location = new System.Drawing.Point(42, 265);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1272, 530);
+            this.panel1.Size = new System.Drawing.Size(1151, 703);
             this.panel1.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1641, 914);
+            this.ClientSize = new System.Drawing.Size(1582, 980);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.HFDLabel);
+            this.Controls.Add(this.hfdLabel);
             this.Controls.Add(this.connectButton);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.stepsTextBox);
@@ -167,7 +165,7 @@
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button connectButton;
-        private System.Windows.Forms.Label HFDLabel;
+        private System.Windows.Forms.Label hfdLabel;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Panel panel1;
--- AutoFocuser/AutoFocuser/Form1.cs
+++ AutoFocuser/AutoFocuser/Form1.cs
@@ -9,11 +9,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using OpenCvSharp;
 using System.Security.Cryptography;
 using AForge.Math.Geometry;
 using AForge;
 using System.Windows;
+using ImageMagick;
+using System.Drawing.Imaging;
+using System.IO;
+using MetadataExtractor.Formats.Exif.Makernotes;
+
+
 
 namespace AutoFocuser
 {
@@ -36,7 +41,7 @@ namespace AutoFocuser
             trackBar1.SmallChange = 1;
             trackBar1.LargeChange = 1;
             trackBar1.UseWaitCursor = false;
-            this.DoubleBuffered= true;
+            this.DoubleBuffered = true;
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
@@ -56,13 +61,13 @@ namespace AutoFocuser
             ProcessImage(image);
         }
 
-
         PictureBox org = new PictureBox();
+
         private void ProcessImage(Bitmap image)
         {
             Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
-            Threshold thresholdFilter = new Threshold(100);
             Bitmap grayImage = grayscaleFilter.Apply(image);
+            AForge.Imaging.Filters.Threshold thresholdFilter = new AForge.Imaging.Filters.Threshold(100);
             Bitmap thresholdImage = thresholdFilter.Apply(grayImage);
 
             // Apply blob counter to detect objects in the image
@@ -70,26 +75,79 @@ namespace AutoFocuser
             blobCounter.FilterBlobs = true;
             blobCounter.MinHeight = 10;
             blobCounter.MinWidth = 10;
-            blobCounter.ObjectsOrder = ObjectsOrder.Size; 
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
             blobCounter.ProcessImage(thresholdImage);
 
             SimpleShapeChecker simpleShapeChecker = new SimpleShapeChecker();
-            GrahamConvexHull grahamConvexHull= new GrahamConvexHull();
-
+            GrahamConvexHull grahamConvexHull = new GrahamConvexHull();
+            float hfd=-1;
             // Get information about objects in the image
             Blob[] blobs = blobCounter.GetObjectsInformation();
-            for (int i = 0, n = blobs.Length; i < n; i++)
+            Bitmap starbg = null;
+            foreach (Blob blob in blobs)
             {
-                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
-                List<IntPoint> hull = grahamConvexHull.FindHull(edgePoints);
+
                 Graphics g = Graphics.FromImage(image);
-                g.DrawPolygon(new Pen(Color.Red, 2), hull.Select(p => new PointF(p.X, p.Y)).ToArray());
+
+                blobCounter.ExtractBlobsImage(thresholdImage, blob, true);
+                Bitmap star = blob.Image.ToManagedImage();
+                //AForge.Imaging.Filters.Invert invertFilter = new AForge.Imaging.Filters.Invert();
+                //star = invertFilter.Apply(star);
+
+                Rectangle expandedRectangle = new Rectangle(
+                blob.Rectangle.X - 200,
+                blob.Rectangle.Y - 200,
+                blob.Rectangle.Width + 400,
+                blob.Rectangle.Height + 400);
+
+                // Ensure the expanded rectangle stays within the bounds of the original image
+                expandedRectangle.Intersect(new Rectangle(0, 0, star.Width, star.Height));
+
+                // Crop the image to just include the expanded bounding rectangle
+                starbg = star.Clone(expandedRectangle, image.PixelFormat);
+
+
+                Blob savedBlob = null;
+                BlobCounter blobCounter2 = new BlobCounter();
+                blobCounter2.FilterBlobs = true;
+                blobCounter2.MinHeight = 10;
+                blobCounter2.MinWidth = 10;
+                blobCounter2.ObjectsOrder = ObjectsOrder.Size;
+                blobCounter2.ProcessImage(starbg);
+                Blob[] blobsInside = blobCounter2.GetObjectsInformation();
+                foreach (Blob blobInside in blobsInside)
+                {
+                    savedBlob= blobInside;
+                    List<IntPoint> edgePoints = blobCounter2.GetBlobsEdgePoints(savedBlob);
+                    List<IntPoint> hull = grahamConvexHull.FindHull(edgePoints);
+                    // Calculate desired circle diameter
+                    int circleDiameter = 300;
+
+                    // Calculate circle bounds
+                    int circleRadius = circleDiameter / 2;
+                    int circleX = (int)savedBlob.CenterOfGravity.X - circleRadius;
+                    int circleY = (int)savedBlob.CenterOfGravity.Y - circleRadius;
+                    hfd = CalcHfd(starbg, circleDiameter);
+                    int circleXHFD = (int)(savedBlob.CenterOfGravity.X - hfd/2);
+                    int circleYHFD = (int)(savedBlob.CenterOfGravity.Y - hfd/2);
+
+                    using (Graphics gh = Graphics.FromImage(starbg))
+                    {
+                        gh.DrawEllipse(new Pen(Color.Red, 2), circleX, circleY, circleDiameter, circleDiameter);
+                        gh.DrawEllipse(new Pen(Color.Green, 2), circleXHFD, circleYHFD, hfd, hfd);
+                        gh.DrawPolygon(new Pen(Color.Blue, 3), hull.Select(p => new PointF(p.X, p.Y)).ToArray());
+                    }
+                }
+
+                starbg.Save("C:\\Users\\mical\\Desktop\\testOutput15.bmp");
+
             }
             // Update picture box
-            pictureBox1.Image = image;
-            org.Image = image;
-            image.Save("C:\\Users\\mical\\Desktop\\testOutput1.bmp");
-            panel1.AutoScrollPosition = new System.Drawing.Point((int)blobs[0].CenterOfGravity.X-panel1.Width/2, (int)blobs[0].CenterOfGravity.Y-panel1.Height/2);
+            pictureBox1.Image = starbg;
+            org.Image = starbg;
+            image.Save("C:\\Users\\mical\\Desktop\\testOutput11.bmp");
+            panel1.AutoScrollPosition = new System.Drawing.Point((int)blobs[0].CenterOfGravity.X - panel1.Width / 2, (int)blobs[0].CenterOfGravity.Y - panel1.Height / 2);
+            hfdLabel.Text="HFD: "+hfd.ToString();
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
@@ -109,6 +167,54 @@ namespace AutoFocuser
             g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
             return bm;
         }
+
+
+        float CalcHfd(Bitmap inImage, int inOuterDiameter)
+        {
+            // Sum up all pixel values in whole circle
+            float outerRadius = inOuterDiameter / 2f;
+            float sum = 0, sumDist = 0;
+            int centerX = (int)Math.Ceiling(inImage.Width / 2f);
+            int centerY = (int)Math.Ceiling(inImage.Height / 2f);
+
+            for (int x = 0; x < inImage.Width; x++)
+            {
+                for (int y = 0; y < inImage.Height; y++)
+                {
+                    if (InsideCircle(x, y, centerX, centerY, outerRadius))
+                    {
+                        sum += GetPixelValue(inImage, x, y);
+                        sumDist += GetPixelValue(inImage, x, y) *
+                                   (float)Math.Sqrt(Math.Pow(x - centerX, 2.0f) + Math.Pow(y - centerY, 2.0f));
+                    }
+                }
+            }
+
+            // NOTE: Multiplying with 2 is required since actually just the HFR is calculated above
+            return (sum != 0 ? 2f * sumDist / sum : (float)Math.Sqrt(2) * outerRadius);
+        }
+
+        bool InsideCircle(float inX, float inY, float inCenterX, float inCenterY, float inRadius)
+        {
+            return Math.Pow(inX - inCenterX, 2.0) + Math.Pow(inY - inCenterY, 2.0) <= Math.Pow(inRadius, 2.0);
+        }
+
+        float GetPixelValue(Bitmap image, int x, int y)
+        {
+            Color pixel = image.GetPixel(x, y);
+            return pixel.GetBrightness();
+        }
+
+
+        public static byte[] BitmapToByteArray(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Bmp);
+                return ms.ToArray();
+            }
+        }
+
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
